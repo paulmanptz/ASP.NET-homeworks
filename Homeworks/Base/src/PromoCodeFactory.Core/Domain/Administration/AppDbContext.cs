70a1ffd26f0b30c98d6d5f9a8edf661c81806727
paulmanptz <pavhumala@gmail.com>
2024-11-29 17:36:42 +0300

    EF test

---
+++ Homeworks/Base/src/PromoCodeFactory.Core/Domain/Administration/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PromoCodeFactory.Core.Domain.Administration
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
--- Homeworks/Base/src/PromoCodeFactory.WebHost/Startup.cs
+++ Homeworks/Base/src/PromoCodeFactory.WebHost/Startup.cs
@@ -1,29 +1,47 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using PromoCodeFactory.Core.Abstractions.Repositories;
 using PromoCodeFactory.Core.Domain.Administration;
 using PromoCodeFactory.DataAccess.Data;
 using PromoCodeFactory.DataAccess.Repositories;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace PromoCodeFactory.WebHost
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSingleton(typeof(IRepository<Employee>), (x) => 
-                new InMemoryRepository<Employee>(FakeDataFactory.Employees));
-            services.AddSingleton(typeof(IRepository<Role>), (x) => 
-                new InMemoryRepository<Role>(FakeDataFactory.Roles));
+            //services.AddSingleton(typeof(IRepository<Employee>), (x) => 
+            //    new InMemoryRepositoryList<Employee>(FakeDataFactory.Employees.ToList()));
+            //services.AddSingleton(typeof(IRepository<Role>), (x) => 
+            //    new InMemoryRepositoryList<Role>(FakeDataFactory.Roles.ToList()));
 
-            services.AddOpenApiDocument(options =>
-            {
-                options.Title = "PromoCode Factory API Doc";
-                options.Version = "1.0";
-            });
+            var connString = Configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<AppDbContext>(
+                options => options.UseSqlite(connString)
+                );
+
+
+            //services.AddOpenApiDocument(options =>
+            //{
+            //    options.Title = "PromoCode Factory API Doc";
+            //    options.Version = "1.0";
+            //});
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
