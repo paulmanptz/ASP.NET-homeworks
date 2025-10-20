1407036534bf411148e1d0662256d91e844edee5
Pavel Khumala <humala@infocraft.ru>
2025-10-20 12:46:36 +0300

    Выполнение ДЗ "Работа с NoSQL"

--- Homeworks/NoSQL/src/Pcf.Administration/Pcf.Administration.DataAccess/Repositories/EfRepository.cs
+++ Homeworks/NoSQL/src/Pcf.Administration/Pcf.Administration.DataAccess/Repositories/EfRepository.cs
@@ -1,18 +1,17 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Pcf.Administration.Core.Abstractions.Repositories;
+using Pcf.Administration.Core.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Query;
-using Pcf.Administration.Core.Abstractions.Repositories;
-using Pcf.Administration.Core.Domain;
 
 namespace Pcf.Administration.DataAccess.Repositories
 {
     public class EfRepository<T>
         : IRepository<T>
-        where T: BaseEntity
+        where T : BaseEntity
     {
         private readonly DataContext _dataContext;
 
@@ -20,24 +19,24 @@ namespace Pcf.Administration.DataAccess.Repositories
         {
             _dataContext = dataContext;
         }
-        
+
         public async Task<IEnumerable<T>> GetAllAsync()
         {
-            var entities = await _dataContext.Set<T>().ToListAsync();
+            List<T> entities = await _dataContext.Set<T>().ToListAsync();
 
             return entities;
         }
 
         public async Task<T> GetByIdAsync(Guid id)
         {
-            var entity = await _dataContext.Set<T>().FirstOrDefaultAsync(x => x.Id == id);
+            T entity = await _dataContext.Set<T>().FirstOrDefaultAsync(x => x.Id == id);
 
             return entity;
         }
 
         public async Task<IEnumerable<T>> GetRangeByIdsAsync(List<Guid> ids)
         {
-            var entities = await _dataContext.Set<T>().Where(x => ids.Contains(x.Id)).ToListAsync();
+            List<T> entities = await _dataContext.Set<T>().Where(x => ids.Contains(x.Id)).ToListAsync();
             return entities;
         }
 
@@ -66,6 +65,16 @@ namespace Pcf.Administration.DataAccess.Repositories
         {
             _dataContext.Set<T>().Remove(entity);
             await _dataContext.SaveChangesAsync();
+        }
+
+        public Task CreateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAllAsync()
+        {
+            throw new NotImplementedException();
         }
     }
 }
